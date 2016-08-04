9a2599ad0fb2c3f7c4d840dcf9a7eb92ab9567c8
byronpolley <byronpolley@live.com>
2016-08-04 10:43:59 +0200

    boilerplate working

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function (grunt) {
+
+    //Checks the dependencies associated with Grunt and autoloads
+    //& requires ALL of them in this Gruntfile
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    // Project configuration.
+    grunt.initConfig({
+
+
+
+
+
+        // Sass configuration
+        sass: {
+            options: {
+                sourceMap: false,
+                outputStyle: 'compressed'
+            },
+            dist: {
+                files: {
+                    'css/main.css': 'css/scss/main.scss'
+                }
+            }
+        },
+
+        wiredep: {
+
+            task: {
+
+                // Point to the files that should be updated when
+                // you run `grunt wiredep`
+                src: [
+      '*.html', // .html support...
+     'css/scss/main.scss',  // .scss & .sass support...
+    ],
+
+                options: {
+                    // See wiredep's configuration documentation for the options
+                    // you may pass:
+
+                    // https://github.com/taptapship/wiredep#configuration
+                }
+            }
+        },
+
+
+        // Copy font awesome fonts into relative project
+        copy: {
+            font_awesome: {
+                expand: true,
+                flatten: true,
+                src: ['bower_components/font-awesome/fonts/*'],
+                dest: 'fonts'
+            }
+        },
+
+
+
+
+
+        //Use PostCSS Autoprefixer to apply browser prefixes for certain styles
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+          require('autoprefixer-core')({
+                        browsers: ['last 2 versions']
+                    })
+        ]
+            },
+            dist: {
+                src: 'css/*.css'
+            }
+        },
+
+
+
+
+
+        //Watches files and folders for us
+        watch: {
+            files: [
+        '*.html',
+        'js/**/*.js',
+        'css/**/*.scss',
+        'img/**/*.{png,jpg,gif,svg}'
+      ],
+            tasks: [
+        'copy',
+        'sass',
+        'wiredep',
+        'postcss'
+      ]
+        }
+
+    });
+
+
+    grunt.loadNpmTasks('grunt-wiredep');
+    //grunt serve
+    grunt.registerTask('default', ['wiredep', 'watch']);
+
+};
